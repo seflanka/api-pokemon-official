f8a7d26cb3665693ce922805ef5a376ee86ac2af
Guilherme de  Souza <91706877+seflanka@users.noreply.github.com>
2022-06-02 06:23:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
+const exphbs = require('express-handlebars')
+const basePath = path.join(__dirname, 'templates' )
 
-const basePath = path.join(__dirname, 'templates')
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+
+app.use(express.static('public'))
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
 app.get('/', (req, res) => {
-    res.sendFile(`${basePath}/index.html`)
+     // res.sendFile(`${basePath}/index.html`)
+
+    res.render('home')
+    
 })
 
 app.listen(port, () => {
